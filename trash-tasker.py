5abdafcfa961d0d4e735627f85f27ab738af136c
Robin Trioux <robin@trioux.eu>
2024-10-26 17:19:56 +0200

    Added schedule csv and glass management

--- trash-tasker.py
+++ trash-tasker.py
@@ -1,5 +1,7 @@
 import ovh
-
+import datetime
+import pandas as pd
+import json
 
 client_get = ovh.Client(config_file="ovh.conf")
 client_send = ovh.Client(config_file="ovh_send.conf")
@@ -26,4 +28,18 @@ def send_sms(number: str, message: str):
 
 
 if __name__ == "__main__":
-    pass
+    td = datetime.datetime.now()
+    _, week, day = td.isocalendar()
+
+    df = pd.read_csv("schedule.csv", sep=",", na_values="")
+    df = df.where(pd.notnull(df), None)  # Replace NaN with None
+    name = df["Nom"][week - 1]
+
+    with open("directory.json", "r") as file:
+        rep = json.load(file)
+        number = rep[name]["phone"]
+        glass_notification = df["Verre"][week - 1] is not None
+        message_glass = "ET VERRES" if glass_notification else ""
+        message = f"RAPPEL POUBELLES {message_glass}: Bonjour {name:.12} c'est votre tour pour les poubelles ce weekend."
+
+        send_sms(rep[name]["phone"], message)
